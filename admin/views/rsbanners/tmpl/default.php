6403b08f0a33bcaa8f253b5e1cabc9f8c12b73e5
diddipoeler <famploeger@arcor.de>
2013-07-12 09:59:07 +0200

    banner anzeigen

--- admin/views/rsbanners/tmpl/default.php
+++ admin/views/rsbanners/tmpl/default.php
@@ -4,5 +4,5 @@ defined('_JEXEC') or die('Restricted access');
 
 echo $this->loadTemplate('company');
 echo $this->loadTemplate('header');
-
+echo $this->loadTemplate('rows');
 echo $this->loadTemplate('footer');
