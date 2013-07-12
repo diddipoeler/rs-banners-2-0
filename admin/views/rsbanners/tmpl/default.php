1c7487e03d14166602ca4c5eff8dcc97d44dd4b5
diddipoeler <famploeger@arcor.de>
2013-07-12 09:23:10 +0200

    template default

--- admin/views/rsbanners/tmpl/default.php
+++ admin/views/rsbanners/tmpl/default.php
@@ -2,6 +2,7 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
+echo $this->loadTemplate('company');
 echo $this->loadTemplate('header');
 
 echo $this->loadTemplate('footer');
