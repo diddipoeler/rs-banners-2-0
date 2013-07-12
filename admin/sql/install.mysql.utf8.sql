d2e4b5aed73e07a4601e397553e545cf108316f4
diddipoeler <famploeger@arcor.de>
2013-07-12 09:03:33 +0200

    sql installations datei

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -5,6 +5,11 @@ CREATE TABLE IF NOT EXISTS `#__rsbanners_ad`
 `cid` int(11) NOT NULL DEFAULT '0', 
 `ad_code` text NOT NULL, 
 `status` int(1) NOT NULL DEFAULT '1',
+`ordering` int(11) NOT NULL DEFAULT '0',
+  `checked_out` int(11) NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` DATETIME NULL ,
+  `modified_by` INT NULL ,
  PRIMARY KEY (`id`) 
  ) 
  ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
@@ -15,6 +20,11 @@ CREATE TABLE IF NOT EXISTS `#__rsbanners_adcat`
  `name` varchar(150) NOT NULL,
   `code` varchar(50) NOT NULL,
    `status` int(1) NOT NULL DEFAULT '1',
+   `ordering` int(11) NOT NULL DEFAULT '0',
+  `checked_out` int(11) NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` DATETIME NULL ,
+  `modified_by` INT NULL ,
     PRIMARY KEY (`id`)
  ) 
  ENGINE=MyISAM  DEFAULT CHARSET=utf8 ; 
