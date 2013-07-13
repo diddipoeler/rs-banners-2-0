e98de6966bf8eb3939b7f4aff1075930a00fed8f
diddipoeler <famploeger@arcor.de>
2013-07-13 11:56:35 +0200

    module

--- site/modules/mod_rsbanners/tmpl/default.php
+++ site/modules/mod_rsbanners/tmpl/default.php
@@ -1,12 +1,13 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-echo 'banner <pre>',print_r($banners, true),'</pre>';
+//echo 'banner <pre>',print_r($banners, true),'</pre>';
 
 foreach ( $banners as $row )
 {
-echo $row->ad_code; 
-$document->addScript($row->ad_code);  
+//echo $row->ad_code; 
+//$document->addScript($row->ad_code);
+echo stripslashes($row->ad_code);  
 }
 
 ?>
