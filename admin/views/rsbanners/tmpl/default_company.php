1b34e1bf2662742dfb0bb95e8d45adac1f02378b
diddipoeler <famploeger@arcor.de>
2013-07-13 13:12:16 +0200

    spende

--- admin/views/rsbanners/tmpl/default_company.php
+++ admin/views/rsbanners/tmpl/default_company.php
@@ -28,7 +28,19 @@ echo JHTML::image(JURI::root().'administrator/components/com_rsbanners/assets/im
 
 <div>
 <p>If you find our FREE Joomla Extensions are useful, please support the development by making a donation. Thank you.</p>
-<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4107842" target="_blank" title="PayPal - The safer, easier way to pay online."><img src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG_global.gif" alt="PayPal - The safer, easier way to pay online." border="0"></a></p>
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post"> 
+<input type="hidden" name="cmd" value="_donations"> 
+<input type="hidden" name="business" value="diddipoeler@arcor.de"> 
+<input type="hidden" name="lc" value="GB"> 
+<input type="hidden" name="item_name" value="Donation to support Knuddelesel.de!"> 
+<input type="hidden" name="no_note" value="0"> 
+<input type="hidden" name="currency_code" value="EUR"> 
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest"> 
+<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online."> 
+<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1"> 
+</form> 
+
 </div>
 
 </div>
