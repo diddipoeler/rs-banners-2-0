e27e032dbf6641a290e3a20eb8738897392c23e2
diddipoeler <famploeger@arcor.de>
2013-07-13 11:49:38 +0200

    module

--- site/modules/mod_rsbanners/tmpl/default.php
+++ site/modules/mod_rsbanners/tmpl/default.php
@@ -5,7 +5,8 @@ echo 'banner <pre>',print_r($banners, true),'</pre>';
 
 foreach ( $banners as $row )
 {
-echo $row->ad_code;   
+echo $row->ad_code; 
+$document->addScript($row->ad_code);  
 }
 
 ?>
